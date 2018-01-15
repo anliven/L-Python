45013859cd79d927182ba9bc2a409ac3877cb954
anliven <anliven@yeah.net>
2018-01-15 23:40:14 +0800

    update Python scripts

--- Python3-Basics/Chapter06Modules.py
+++ Python3-Basics/Chapter06Modules.py
@@ -11,17 +11,26 @@ print('Version', Chapter06ModuleTest.__version__)
 TestPackage.ModuleTest.say_hello()
 print('Version', TestPackage.ModuleTest.__version__)
 
-print('\nThe PYTHONPATH is', sys.path)
-print("Square root of 36 is", sqrt(36))
+print('\nThe PYTHONPATH is', sys.path)  # 通过“模块名.方法名”方式调用模块里的方法
+print("Square root of 36 is", sqrt(36)) # 通过“from...import...语句”引入模块的指定函数，调用时不需要加模块名；
 
 print(dir())
 print(dir(sys))
 
 # ### 模块（Modules）
-# - 模块可以被其它程序导入并运用其功能；
-# - 最简单模块是包含函数与变量，以.py为后缀的文件；
+# - 模块是一个包含函数和变量的.py文件，可以被其它程序通过import方式导入，从而使用该模块中的函数等功能，提升代码复用性；
 # - 默认情况下，导入文件或者模块，Python解释器将在sys.path里找其路径，如果没有则报错；
+#
+# ### 引入模块
+# - 可以通过“import 语句”或“from…import… 语句”引入模块；
+# - 通过“import 语句”引入模块后，必须通过“模块名.方法名”方式调用模块的方法；
+# - 通过“from...import...语句”导入模块的指定函数后，调用方法时不需要加模块名；
+# - “from support import *”可以引入support模块的所有函数，作用与“import support”一致；
 # - 为了使程序更加易读和避免名称冲突，建议使用import语句导入模块，而不是from...import语句；
+#
+# ### 同时引入多个模块
+# - “import 语句”格式：import module1,module2,...；
+# - “from...import...语句”可以同时引入多个模块的多个方法，当引入不同模块的相同方法名时，以最后引入的方法为准；
 #
 # ### dir()
 # - 内置dir()函数能够返回由对象所定义的名称列表，能够对任何对象工作；
--- Python3-Basics/Chapter09ObjectOriented1Class.py
+++ Python3-Basics/Chapter09ObjectOriented1Class.py
@@ -13,24 +13,60 @@ class Person2:
     def sayhi(self):
         print('Hello, my name is', self.name)
 
+
 p = Person1()
 print(p)  # 打印结果，类的__main__产生了一个实例，以及这个实例对象的内存地址；
 
 p = Person2('Anliven')
 p.sayhi()
-# Person2('Anliven').sayhi()  # 等同于上两行
+Person2('Anliven').sayhi()  # 等同于上两行
+
+
+class Rectangle:
+    width = 0  # 定义类的属性
+    height = 0
+
+    __color = ""  # 定义类的私有属性
+
+    def __init__(self, width, height, color):  # 定义构造函数
+        self.width = width
+        self.height = height
+        self.__color = color
+
+    def __getArea(self):  # 定义类的私有方法
+        return self.width * self.height
+
+    def printme(self):  # 定义类的方法；类内部可调用类的私有属性和私有方法
+        print("width = %d,height = %d,area = %d,color = %s" % (self.width, self.height, self.__getArea(), self.__color))
+
+
+R = Rectangle(3, 4, "blue")  # 创建类的实例，自动调用__init__()构造方法
+print("width =", R.width)  # 访问实例的属性
+R.printme()  # 访问实例的方法
+# print("color = %s, Area = %d " % (R.__color, R.__getArea()))  # 报错，因为实例无法访问类的私有属性和私有方法
 
 # ### 类与对象
+# - 类(Class): 用来描述具有相同的属性和方法的对象的集合，定义了该集合中每个实例所共有的属性和方法。
 # - 通过class关键字创建类；
 # - 对象（Object）是类的实例（Instance）；
-# - 字段与方法通称类的属性（Attribute）；
+#
+# ### 类的属性（Attribute）
+# - 类的属性包括字段与方法；
 # - 字段（Field）：对象或类的变量，分别被称为实例变量（Instance Variables）与类变量（Class Variables）；
-# - 方法（Method）：在类中实现某些功能的函数，可以被对象使用；
+# - 将属性名以双下划线开头可将属性定义为私有属性，表示其只能在类内部访问以self.的方式访问，类的实例（实例）无法访问；
+#
+# ### 类的方法（Method）
+# - 在类中使用def关键字定义的函数，类方法必须包含参数self且为第一个参数，self代表的是类的实例；
+# - 方法名以双下划线开头可将方法定义为私有方法，表示表示其只能在类内部访问以self.的方式访问，类的实例（实例）无法访问；
 #
 # ### self
 # - 简单的说，self参数是指类实例自身（也就是对象本身）的引用；
 # - 在定义类方法(类中的函数)时，self参数必须存在并且只能作为第一个参数；
 # - 即使类的方法不需要任何参数，self也必须存在类的方法中，而独立的函数或方法则不需要；
+#
+# ### __init__()
+# 类的构造方法__init__()会在创建类实例的同时初始化实例；
+# 构造方法__init__()的第一个参数必须为self，在创建类的实例时，会自动调用该构造方法；
 #
 # ### 举例说明
 # - 创建了一个类MyClass，实例化对象为MyObject，然后调用对象的方法MyObject.method(a,b)；

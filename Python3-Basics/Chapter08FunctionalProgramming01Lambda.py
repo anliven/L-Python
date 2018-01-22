bd78205bb4bdd7ddc4a6235e76e5826db398df8c
anliven <anliven@yeah.net>
2018-01-23 07:56:16 +0800

    add and update Python3 scripts

--- Python3-Basics/Chapter08FunctionalProgramming01Lambda.py
+++ Python3-Basics/Chapter08FunctionalProgramming01Lambda.py
@@ -2,25 +2,37 @@
 # -*- coding: utf-8 -*-
 
 
-def multiply(x, y):  # 显式定义一个求两数乘积的函数
+def func(x):  # 显式定义函数
+    return x * x
+
+
+print(list(map(func, [1, 2, 3, 4, 5, 6, 7, 8, 9])))  # 调用自定义函数
+print(list(map(lambda x: x * x, [1, 2, 3, 4, 5, 6, 7, 8, 9])))  # 匿名函数ambda的形式
+
+
+def multiply(x, y):
     return x * y
 
 
 print(multiply(16, 18))
 
-func1 = lambda x, y: x * y  # 匿名函数实现"求两数乘积"，但PEP8不建议这样的写法
+func1 = (lambda x, y: x * y)  # 匿名函数赋值给一个变量，再利用变量来调用该函数
+print(func1)  # 此时func1是一个匿名函数对象
 print(func1(16, 18))
 
 
 def func2():
-    return lambda x, y: x * y  # 匿名函数实现"求两数乘积"，并作为返回值返回
+    return lambda x, y: x * y  # 匿名函数作为返回值返回
 
 
-f = func2()  # ???
+f = func2()  # ？
+print(f)  # 此时f是一个匿名函数对象
 print(f(16, 16))
 
+# ### 匿名函数（lambda）
 # 如果函数比较简单且复用性很低，可以将其简洁地定义为匿名函数，不需要显式定义；
 # 用lambda关键字来创建匿名函数，语法格式："lambda arg1,arg2,...: expression"；
 #
-# 1.lambda函数拥有自己的命名空间，且不能访问自有参数列表之外或全局命名空间里的参数;
-# 2.lambda的主体是一个表达式，而不是一个代码块，只能在lambda表达式中封装有限的逻辑;
+# lambda函数拥有自己的命名空间，且不能访问自有参数列表之外或全局命名空间里的参数;
+# lambda的主体是一个表达式，而不是一个代码块，只能在lambda表达式中封装有限的逻辑;
+# 匿名函数只能有一个表达式，不用写return，返回值就是该表达式的结果;
---
+++ Python3-Basics/Chapter08FunctionalProgramming05Sorted.py
@@ -0,0 +1,38 @@
+#! python3
+# -*- coding: utf-8 -*-
+
+list_sample = [666, 555, -111, 999, -222]
+print(sorted(list_sample))
+print(sorted(list_sample, key=abs))  # ？
+print(list_sample)
+
+list_sample2 = ['bbb', 'aaa', 'ccc', 'ZZZ', 'XXX', 'YYY']
+print(sorted(list_sample2))
+print(sorted(list_sample2, key=str.lower))  # 忽略大小写排序
+print(sorted(list_sample2, key=str.lower, reverse=True))  # 忽略大小写反向排序
+
+list_sample3 = [('BBB', 77), ('AAA', 99), ('RRR', 66), ('LLL', 88)]
+
+
+def by_name(t):
+    return t[0].lower()
+
+
+def by_score(t):
+    return t[1]
+
+
+print('Sorted by name: ', sorted(list_sample3, key=by_name))
+print('Sorted by score: ', sorted(list_sample3, key=by_score, reverse=False))  # reverse=False为升序排序
+
+# ### sorted函数
+# Python内置的sorted()函数可以对list进行排序,返回一个新的列表，不改动原先列表；
+# 可以接收一个key函数来实现自定义的排序，key指定的函数将作用于list的每一个元素上，并根据key函数返回的结果进行排序；
+# 对字符串排序，默认是按照ASCII的大小进行比较，但可以通过key函数实现自定义的排序；
+# 使用sorted()排序的关键在于实现一个映射函数；
+#
+# ### 对比sort()函数
+# - 对于一个无序的列表a，调用a.sort()，对a进行排序后返回a，sort()函数修改原列表内容；
+# - 对于一个无序的列表a，调用sorted(a)，对a进行排序后返回一个新的列表，而对原列表内容不产生影响；
+#
+# - 当列表由list（或者tuple）组成时，默认情况下，sort和sorted都会根据list[0]（或者tuple[0]）作为排序的key，进行排序;

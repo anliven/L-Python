166d711df54d63efb43d5c27bb7bc1bb0283c4a6
anliven <anliven@yeah.net>
2018-01-24 23:41:05 +0800

    update Python3 scripts

--- Python3-Basics/Chapter06ModuleTest.py
+++ Python3-Basics/Chapter06ModuleTest.py
@@ -7,6 +7,21 @@ def say_hi():
     print('Hi, Chapter06ModuleTest.')
 
 
+def _private_1(name):
+    print('Hello, %s' % name)
+
+
+def _private_2(name):
+    print('Hi, %s' % name)
+
+
+def greeting(name):
+    if len(name) > 3:
+        return _private_1(name)
+    else:
+        return _private_2(name)
+
+
 if __name__ == '__main__':  # 判断是否是在直接运行
     print('This program is being run by itself. __name__ ： ', __name__)
 else:
@@ -20,4 +35,12 @@ else:
 # ### 自定义模块位置
 # - 自定义模块应该放置在sys.path所列出的其中一个目录下，或者自定义包中；
 # - 也可以与引用模块在同一目录，这种方式最简单，但很少使用这样扁平化的结构；
-
+#
+# ### 模块的作用域
+# 正常的函数和变量是公开的（public），可以被直接引用；
+# 通过_前缀来实现的类似__xxx__的变量是有特殊用途的变量，但可以被直接引用；
+# 通过_前缀来实现的类似_xxx和__xxx的函数和变量是非公开的（private），从编程习惯上不应该被直接引用，建议仅在模块内部使用；
+# Python中无法完全限制访问private函数或变量，仅仅是从编程习惯上建议“不应该”被直接引用；
+#
+# ### 一种常用的代码封装和抽象
+# 外部不需要引用的函数全部定义成private，只有外部需要引用的函数才定义为public;
--- Python3-Basics/Chapter06Modules.py
+++ Python3-Basics/Chapter06Modules.py
@@ -12,14 +12,17 @@ TestPackage.ModuleTest.say_hello()
 print('Version', TestPackage.ModuleTest.__version__)
 
 print('\nThe PYTHONPATH is', sys.path)  # 通过“模块名.方法名”方式调用模块里的方法
-print("Square root of 36 is", sqrt(36)) # 通过“from...import...语句”引入模块的指定函数，调用时不需要加模块名；
+print("Square root of 36 is", sqrt(36))  # 通过“from...import...语句”引入模块的指定函数，调用时不需要加模块名
 
-print(dir())
-print(dir(sys))
+print('dir(): ', dir())
+print('dir(sys): ', dir(sys))
+
+Chapter06ModuleTest.greeting('Anliven')
 
 # ### 模块（Modules）
 # - 模块是一个包含函数和变量的.py文件，可以被其它程序通过import方式导入，从而使用该模块中的函数等功能，提升代码复用性；
 # - 默认情况下，导入文件或者模块，Python解释器将在sys.path里找其路径，如果没有则报错；
+# - 可以通过设置环境变量PYTHONPATH来指定搜索目录；
 # - 模块名不能使用特殊字符和中文，更不能与系统模块名冲突；
 #
 # ### 使用模块的好处
@@ -47,3 +50,12 @@ print(dir(sys))
 # - 内置dir()函数能够返回由对象所定义的名称列表，能够对任何对象工作；
 # - 默认情况（没有提供参数），dir函数将返回当前模块的属性列表；
 # - 如果参数是模块名称，函数将返回这一指定模块的名称列表；
+#
+# ### 第三方模块
+# 通过包管理工具pip安装第三方模块;
+# 通过PyPI（The Python Package Index）获取:https://pypi.python.org/pypi
+#
+# ### 安装常用模块
+# Anaconda是一个基于Python的数据处理和科学计算平台，内置了许多非常有用的第三方库，简单易用；
+# Anaconda官网：https://www.anaconda.com/
+# 安装Anaconda完成后，Path环境变量将指向Anaconda自带的Python，其内置的第三方模块安装在自己的路径下，不影响系统已安装的Python目录；

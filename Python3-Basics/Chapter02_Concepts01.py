e4d69326a3bf688dc0851ac7973070aa73ac9d8e
anliven <anliven@yeah.net>
2018-03-23 00:18:23 +0800

    rename and add pytyhon3 scripts

--- Python3-Basics/Chapter02_Concepts01.py
+++ Python3-Basics/Chapter02_Concepts01.py
@@ -5,7 +5,7 @@ print('abc')
 print("abc'ABC")  # 双引号之间可以包含单引号
 print('''abc'ABC"123''')  # 三引号之间可以包含单引号和双引号
 print(111, '222', "333", '''444''', """555""")  # 多个字符串用逗号“,”隔开，逗号“,”会输出一个空格
-print('100 + 100 =', 100 + 100)  # 计算结果
+print('(100 + 100)*3 - 45 =', (100 + 100) * 3 - 45)  # 数值计算
 
 name = 'Anliven'  # 声明变量，使用赋值运算符（=）赋值给变量
 age = 29
@@ -16,7 +16,7 @@ print("%s was %s years old." % (name, age))  #
 print('{0} was {1} years old.'.format(name, age))
 print('{} was {} years old.'.format(name, age))  # 索引数字是可选项
 
-print('{0:.3f}'.format(10.0/3))  # 对于浮点数保留小数点(.)后三位
+print('{0:.3f}'.format(10.0 / 3))  # 对于浮点数保留小数点(.)后三位
 print('{0:_^9}'.format('WOW'))  # 使用下划线填充文本，并保持文字处于中间位置；使用 (^) 定义字符串长度为9
 
 print('This is the 1st line.\nThis is the 2nd line.')  # 换行显示
@@ -33,20 +33,20 @@ print("abc")  # print()函数默认会换行，可以使用语法print(str,end='
 print('ABC', end=' ')  # 以空格结尾，不换行
 print('123')
 
-x = "hi"  # Python中的变量不需要声明类型，其在使用前必须赋值
+x = "hi"  # Python中的变量不需要声明类型，但在使用前必须赋值
 if not isinstance(x, (int, float)):  # 内置函数isinstance()实现数据类型检查
     print('Bad operand type.')
 
 a = b = c = 1
 i, j, k = 3.5, 'hello', "python"  # 多个变量同时赋值
 print(x, a, b, c, i, j, k)
+print(id(a) == id(b) == id(c), id(i) == id(j) == id(k))  # id()查看在内存中的地址
 
 a = 555  # 重新赋值
 b = 888
 print('a:', a, 'b:', b)
 a, b = b, a  # the fastest way to swap two variables in Python
 print('a:', a, 'b:', b)
-
 
 # ### 一些定义
 # - 字面常量（Literal Constants）：表示本身字面意义上的值或内容，常量不能被改变，通常用全部大写的变量名表示；
@@ -67,20 +67,26 @@ print('a:', a, 'b:', b)
 # ### Python从0开始计数，索引中的第一位是0，第二位是1，以此类推；
 #
 # ### 变量（Variables）
-# Python中的变量不需要声明类型，其在使用前必须赋值
-# Python允许多个变量同时赋值
+# Python中的变量不需要声明类型，但在使用前必须赋值;
+# Python允许多个变量同时赋值;
 # 等号=是赋值语句，可以把任意数据类型赋值给变量，可以反复赋值，而且可以赋不同类型的值;
+# 注意：
+# - 变量其实是值的“标签”，值才是内存中的数据，而“赋值”实际是建立“标签”和“内存数据”的关系；
+# - 一个“内存数据”可以有多个“标签”（利用id()函数可以查看到，这些“标签”都指向同一个内存地址）；
 #
 # ### 变量的标识符（Identifiers）
-# - 首字符必须是字母或下划线（_）；
-# - 标识符的其它部分是字符、下划线（_）、数字（0~9）；
-# - 标识符名称区分大小写；
+# 变量的标识符也就是变量的名字，必须遵循如下规则：
+# - 由字母、下划线（_）、数字（0~9）组成；
+# - 不能以数字开头，也就是说，首字符必须是字母或下划线（_）；
+# - 区分大小写；
+# - 不能使用关键字；
 #
 # ### 数据类型（Data Type）
 # - 变量可以将各种形式的值保存为不同的数据类型；
 # - python具有基本的数据类型，也可以通过类（Classes）创建自定义的数据类型；
 # - 内置的数据类型转换函数：int(),str(),chr(),float(),bool(),hex()等;
 # - 内置函数isinstance()实现数据类型检查;
+# - 内置函数id()查看变量的值在内存中的地址；
 #
 # ### 转义序列（Escape Sequence）
 # - 通过反斜杠（\）可以转义特殊字符；
--- Python3-Basics/Chapter02_Concepts02.py
+++ Python3-Basics/Chapter02_Concepts02.py
@@ -8,6 +8,7 @@ def is_palindrome(text):  # 判断是否是回文
 
 
 something = input("Enter text: ")  # Python交互式命令行：标准键盘输入
+# print(type(something))  # 内置函数type()查看对象类型
 if is_palindrome(something):
     print("Yes, it is a palindrome")
 else:
@@ -18,7 +19,8 @@ else:
 # - 使用str类的方法可以操作输入的内容，可通过help(str)了解更多信息；
 #
 # ### 标准键盘输入
-# 内置函数input()来实现标准键盘输入。input()可以接收一行文本，并将返回该文本；
+# 内置函数input()来实现标准键盘输入；
+# input()可以接收一行文本，并将返回该文本(注意：返回的是str类型)；
 #
 # ### 标准输出
 # 内置函数print()来实现标准输出；

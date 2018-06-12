1f31d9e8ee153cfb3d4ea538a8b131c7735b46d4
anliven <anliven@yeah.net>
2018-06-13 07:29:25 +0800

    add new python3 scripts

---
+++ Python3-Basics/Chapter10_InputOutput05_json01.py
@@ -0,0 +1,35 @@
+#! python3
+# -*- coding: utf-8 -*-
+import json
+
+test_data = dict(name='测试', age=29, sex="男")
+print("json.dumps: ", json.dumps(test_data, indent=4, ensure_ascii=False))  # indent参数：格式化保存，默认为None(零个空格)
+
+test_data2 = [{'name': '开发', 'age': 30, 'sex': '男'}, {'name': '运维', 'age': 28, 'sex': '女'}]
+with open("test.json", "w", encoding='utf-8') as f:
+    json.dump(test_data2, f, sort_keys=True, indent=4, ensure_ascii=False)  # sort_keys参数：是否对dict的key进行排序
+
+with open("test.json", "r", encoding='utf-8') as f:
+    print("json.dump: ", f.read())
+    f.seek(0)
+    aaa = json.loads(f.read())
+    f.seek(0)
+    bbb = json.load(f)
+print("json.loads: ", aaa)
+print("json.load: ", bbb)
+
+# ### JSON
+# JSON(JavaScript Object Notation, JS对象标记) 是一种轻量级的数据交换格式，相对于XML而言，更加简洁、易于读写，也易于解析和生成；
+# 采用完全独立于编程语言的文本格式来存储和表示数据，表示出来就是一个字符串，可以被所有语言读取，易于存储和通过网络传输；
+# JSON编码是UTF-8，所以总是能正确地在Python的str与JSON字符串之间转换;
+#
+# ### 标准库json模块
+# https://docs.python.org/3/library/json.html;
+# 内置json模块可以完成Python对象和JSON格式之间的转换；
+# - json.dumps()：无文件操作；将Python对象转换成json字符串；
+# - json.dump()：序列化并写入文件；必须传文件描述符，直接将Python对象转换成json字符串并存储到文件中；
+# - json.loads()：无文件操作；将json字符串转换成Python对象，也就是json字符串反序列化；
+# - json.load()：读文件并反序列化；只接收文件描述符，从指定文件中读取json字符串并反序列化;
+#
+# ### 中文编码
+# 在dumps()或dump()函数中设置“ensure_ascii=False”参数，可以避免在序列化时中文被转换为unicode码；
---
+++ Python3-Basics/Chapter10_InputOutput05_json02.py
@@ -0,0 +1,62 @@
+#! python3
+# -*- coding: utf-8 -*-
+import json
+from datetime import date
+from datetime import datetime
+
+
+class Student(object):  # 自定义数据类型
+    def __init__(self, name, age, score):
+        self.name = name
+        self.age = age
+        self.score = score
+
+    def __repr__(self):
+        return 'Student [name: %s, age: %d, score: %d]' % (self.name, self.age, self.score)
+
+
+def stu2dict(std):  # 自定义数据类型的序列化转换函数
+    return {
+        'name': std.name,
+        'age': std.age,
+        'score': std.score
+    }
+
+
+def dict2stu(std):  # 自定义数据类型的反序列化转换函数
+    return Student(std['name'], std['age'], std['score'])
+
+
+test_data = Student('AAA', 23, 99)
+print("New Data Type: ", test_data)
+# print(json.dumps(test_data))  # 自定义数据类型默认不能直接json序列化，会引发TypeError错误
+
+print(json.dumps(test_data, default=stu2dict))
+print(json.dumps(stu2dict(test_data)))  # 序列化的转换过程：“Python对象 --> dict --> JSON object”
+print(json.dumps(test_data, default=lambda obj: obj.__dict__))
+
+test_data2 = '{"name": "BBB", "age": 22, "score": 88}'
+print(json.loads(test_data2, object_hook=dict2stu))  # object_hook函数将dict转换为Python对象
+print(dict2stu(json.loads(test_data2)))  # 反序列化的转换过程：“JSON object -> dict --> Python对象”
+
+
+class MyJsonEncoder(json.JSONEncoder):
+
+    def default(self, obj):
+        if isinstance(obj, datetime):
+            return obj.strftime('%Y-%m-%d %H:%M:%S')
+        elif isinstance(obj, date):
+            return obj.strftime('%Y-%m-%d')
+        else:
+            return json.JSONEncoder.default(self, obj)
+
+
+test_data3 = datetime.now()
+print("DateTime Type: ", test_data3)
+# print(json.dumps(test_data3))  # datetime类型数据默认不能直接json序列化，会引发TypeError错误
+print(json.dumps(test_data3, cls=MyJsonEncoder))
+
+# ### 自定义数据类型
+# 实现自定义数据类型的序列化与反序列化有两种方式：
+# - 编写转换函数；
+# - 继承JSONEncoder和JSONDecoder类；

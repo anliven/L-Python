76a79a947e33c2e15db06d494e8a008ec5fff751
anliven <anliven@yeah.net>
2018-06-20 07:34:53 +0800

    update and add python3-beginner scripts

--- Python3-Beginner/Chapter06_MetaClass03_sample.py
+++ Python3-Beginner/Chapter06_MetaClass03_sample.py
@@ -1,7 +1,7 @@
 # coding=utf-8
 
 
-class ListMetaclass(type):  # 定义一个元类，类名默认以Metaclass结尾；从type继承，用来创建类
+class ListMetaclass(type):  # 定义一个元类，类名默认以Metaclass结尾；metaclass是类的模板，所以必须从`type`类型派生
 
     def __new__(mcs, name, bases, attrs):  # 自定义__new__()方法
         attrs['add'] = lambda self, value: self.append(value)
@@ -9,26 +9,33 @@ class ListMetaclass(type):  # 定义一个元类，类名默认以Metaclass结�
         # return type.__new__(cls, name, bases, attrs)
 
 
-class MyList(list, metaclass=ListMetaclass):  # 指定元类
+class MyList(list, metaclass=ListMetaclass):  # 第一个参数是父类，第二个参数是metaclass指示使用元类来定制类
     pass
 
 
-L = MyList()
+L = MyList()  # 这里Python解释器在创建MyList时，实际通过ListMetaclass.__new__()来创建
 L.add(123)  # 普通的List类型没有add()方法
 L.add(456)
 L.add(789)
 print(L)
 
 # ### 元类（metaclass）
-# 元类（metaclass）是用来创建类（对象）的可调用对象；
-# 简而言之，类是实例对象的模板，元类是类的模板；也就是说，类创建实例，元类创建类；
+# 元类（metaclass）是用来动态创建类（对象）的可调用对象，先定义metaclass，然后根据metaclass创建类，最后根据类创建实例；
+# 简而言之，元类是类的模板，类是实例对象的模板；也就是说，元类创建类，类创建实例；
 # 一般情况下，使用类作为元类，通过使用元类，可以控制类的创建行为，自动地改变类；
 # 元类会隐式地继承到子类，即使没有显示地在子类使用“__metaclass__”；
 #
 # ### 特殊方法“__new__”
 # 元类的操作在特殊方法“__new__”中完成；
 # “__new__”用来创建对象并返回创建后的对象，在“__init__”之前被调用，参数解释如下：
-# - cls：将要创建的类
-# - name：类的名字
-# - bases：类的父类集合
-# - attrs：类的属性和方法，是一个字典
+# - cls：将要创建的类；
+# - name：类的名字；
+# - bases：类的父类集合；
+# - attrs：类的属性和方法，是一个字典；
+#
+# ### 动态修改的意义
+# 一般情况下不会使用动态修改，只适用于某些特殊的场景，能够极大地精简代码；
+# 例如：编写ORM框架；
+#
+# ### ORM（Object Relational Mapping，对象-关系映射），
+# 把关系数据库的一行映射为一个对象，也就是一个类对应一个表，这样写代码更简单，不用直接操作SQL语句；
